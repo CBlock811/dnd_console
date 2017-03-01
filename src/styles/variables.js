fe7e800301fd9e420a6e96ca5e0a24a21bb5574b
Christian <christian.block@yahoo.com>
2017-03-01 14:18:32 -0800

    initial commit

---
+++ src/styles/variables.js
@@ -0,0 +1,29 @@
+const UNIT_SIZE = 8;
+
+export const spacing = {
+  sm: `${UNIT_SIZE * 1}px`,
+  md: `${UNIT_SIZE * 2}px`,
+  lg: `${UNIT_SIZE * 3}px`,
+  xl: `${UNIT_SIZE * 4}px`
+};
+
+export const typeSize = {
+  sm: "0.8125rem",
+  md: "1rem",
+  lg: "1.5rem",
+  xl: "2rem"
+};
+
+export const typeWeight = {
+  light: 300,
+  regular: 400,
+  bold: 700
+};
+
+export const colors = {
+  dark: '#262626',
+  light: '#F2F2F2',
+  primary: '#AB966F',
+  complementary: '#37DEAA',
+  accent: '#E4B928'
+};
